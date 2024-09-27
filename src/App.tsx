727edb8021922043bae5e9ab88483f632784e3a5
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-09-27 21:02:23 +0600

    Modified and fixed tailwind.config, package.json, create MobileHeader components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import MobileHeader from "./components/MobileHeader";
 
 const App: React.FC = (): JSX.Element => {
   return (
     <>
-      
+      <MobileHeader/>
     </>
   );
 };
