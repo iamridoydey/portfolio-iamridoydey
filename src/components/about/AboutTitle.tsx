46b018993e8ba9c70bacad5febdb5e8bf00a4ab0
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-10-16 14:09:15 +0600

    Add animation to AboutTitle.tsx

--- src/components/about/AboutTitle.tsx
+++ src/components/about/AboutTitle.tsx
@@ -1,11 +1,37 @@
-import React, { useContext } from 'react'
-import { ThemeContext } from '../../store/ThemeContext';
+import React, { useContext } from "react";
+import { ThemeContext } from "../../store/ThemeContext";
+import { motion } from "framer-motion";
+import { useWindowWidth } from "../../store/WindowContext";
 
-const AboutTitle:React.FC = () => {
+const AboutTitle: React.FC = () => {
   const isDark = useContext(ThemeContext)?.isDark;
+  const { width } = useWindowWidth();
+
+  const variants = {
+    initial: { x: "-100%", opacity: 0 },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: { duration: 0.5, delay: 0.2, staggerChildren: 0.2 },
+    },
+  };
+
   return (
-    <section className='w-full'><h1 className={`font-poppins text-center text-2xl md:text-3xl font-bold my-10 ${isDark? "text-blue-100": "text-black"}`}>Here Are Few Quotes about myself</h1></section>
-  )
-}
+    <motion.section
+      className="w-full"
+      variants={variants}
+      initial={width >= 1024 ? "initial" : "animate"}
+      whileInView="animate"
+    >
+      <h1
+        className={`font-poppins text-center text-2xl md:text-3xl font-bold my-10 ${
+          isDark ? "text-blue-100" : "text-black"
+        }`}
+      >
+        Here Are Few Quotes about myself
+      </h1>
+    </motion.section>
+  );
+};
 
 export default AboutTitle;
