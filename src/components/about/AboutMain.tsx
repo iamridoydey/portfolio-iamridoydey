6a35af7a1c786c4780c4ab5d240974c579d66dae
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-10-16 14:03:48 +0600

    Animation added to the AboutMain.tsx

--- src/components/about/AboutMain.tsx
+++ src/components/about/AboutMain.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 import AboutLeft from "./AboutLeft";
 import AboutRight from "./AboutRight";
+import { useWindowWidth } from "../../store/WindowContext";
+import { motion } from "framer-motion";
 
 const AboutMain: React.FC = () => {
+  const { width } = useWindowWidth();
+
+  const variants = {
+    initial: { scale: 0.7, opacity: 0.9 },
+    animate: {
+      scale: 1,
+      opacity: 1,
+      transition: { duration: 1, delay: 0.4 },
+    },
+  };
   return (
-    <section className="flex flex-col md:flex-row items-center justify-between gap-8 lg:p-4">
+    <motion.section
+      className="flex flex-col md:flex-row items-center justify-between gap-8 lg:p-4"
+      variants={variants}
+      initial={width >= 1024 ? "initial" : "animate"}
+      whileInView="animate"
+    >
       <AboutLeft />
       <AboutRight />
-    </section>
+    </motion.section>
   );
 };
 
