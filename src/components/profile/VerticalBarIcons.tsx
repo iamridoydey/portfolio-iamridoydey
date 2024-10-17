574881ca20c348a8609454b4b46641486e61253e
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-10-17 19:31:39 +0600

    Replace email icon to github icon

--- src/components/profile/VerticalBarIcons.tsx
+++ src/components/profile/VerticalBarIcons.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaLinkedinIn, FaXTwitter } from "react-icons/fa6";
-import { IoMailOutline } from "react-icons/io5";
+import { IoLogoGithub } from "react-icons/io";
 
 
 
@@ -9,20 +9,20 @@ const VerticalBarIcons: React.FC = () => {
     <section className='absolute top-[-145px] left-5 h-[40%] flex flex-col items-center gap-3' >
       <div className="w-1 h-[60%] bg-blue-300"></div>
       <div>
-        <ul className='flex flex-col gap-2 text-tertiary text-xl'>
+        <ul className='flex flex-col gap-2 text-tertiary text-xl font-bold'>
           <li>
-            <a href="https://www.linkedin.com/in/iamridoydey/">
+            <a href="https://www.linkedin.com/in/iamridoydey/" target='_blank'>
               <FaLinkedinIn />
             </a>
           </li>
           <li>
-            <a href="https://x.com/iamridoy_dey/">
+            <a href="https://x.com/iamridoy_dey/" target='_blank'>
               <FaXTwitter />
             </a>
           </li>
           <li>
-            <a href="mailto:iamridoydey@gmail.com">
-              <IoMailOutline />
+            <a href="https://github.com/iamridoydey" target='_blank'>
+             <IoLogoGithub/>
             </a>
           </li>
         </ul>
